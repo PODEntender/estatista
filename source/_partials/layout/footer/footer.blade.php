7dbeb168fab0b1ab4155e7a2a6271558f0109389
Nawarian <nickolas@phpsp.org.br>
2019-02-26 09:19:05 +0100

    Adding dummy footer

--- source/_partials/layout/footer/footer.blade.php
+++ source/_partials/layout/footer/footer.blade.php
@@ -1,3 +1,34 @@
-<footer>
-    © PODEntender - Todos os direitos reservados.
+<footer class="footer">
+    <nav class="footer__menu">
+        <ul class="footer__menu-group">
+            <li class="footer__menu-item">Início</li>
+            <li class="footer__menu-item">Episódios</li>
+            <li class="footer__menu-item">Contato</li>
+        </ul>
+        <ul class="footer__menu-group">
+            <li class="footer__menu-item">
+                Facebook
+            </li>
+            <li class="footer__menu-item">
+                Instagram
+            </li>
+            <li class="footer__menu-item">
+                Twitter
+            </li>
+        </ul>
+        <ul class="footer__menu-group">
+            <li class="footer__menu-item">
+                iTunes
+            </li>
+            <li class="footer__menu-item">
+                Spotify
+            </li>
+            <li class="footer__menu-item">
+                Deezer
+            </li>
+        </ul>
+    </nav>
+    <div class="footer__copy">
+        © PODEntender - Todos os direitos reservados.
+    </div>
 </footer>
