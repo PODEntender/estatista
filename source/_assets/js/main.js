a5bb1218aedc8d81bc5734304e0afcd63735f2f9
Nawarian <nickolas@phpsp.org.br>
2019-02-27 00:10:14 +0100

    Adding lazy loaded images

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,4 +1,17 @@
 window.toggleMenu = () => document
   .querySelector('.side-menu')
   .classList
-  .toggle("side-menu--visible");
+  .toggle('side-menu--visible');
+
+window.imagesObserver = new IntersectionObserver((entries, observer) => {
+  entries.filter((entry) => entry.isIntersecting).forEach((entry) => {
+    const target = entry.target;
+    target.setAttribute('src', target.getAttribute('data-src'));
+
+    observer.unobserve(target);
+  });
+}, {
+  rootMargin: '-30px',
+});
+
+document.querySelectorAll('img[data-src]').forEach((img) => window.imagesObserver.observe(img));
