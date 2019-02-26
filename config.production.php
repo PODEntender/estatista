243d3c899b40e5ac66e87eca45f9769af2d42f5d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-26 10:03:13 +0000

    Deploy PODEntender/blog to github.com/PODEntender/blog.git:gh-pages

--- config.production.php
+++ config.production.php
@@ -2,5 +2,6 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'https://www.podentender.com',
+    // 'baseUrl' => 'https://www.podentender.com',
+    'baseUrl' => 'https://podentender.github.io/blog/public_html'
 ];
