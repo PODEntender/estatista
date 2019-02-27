c0f03008f1a52cbfef07a8892ac92eed0bb4fd78
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-27 10:06:41 +0000

    Deploy PODEntender/blog to github.com/PODEntender/blog.git:gh-pages

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -3,15 +3,21 @@ window.toggleMenu = () => document
   .classList
   .toggle('side-menu--visible');
 
-window.imagesObserver = new IntersectionObserver((entries, observer) => {
-  entries.filter((entry) => entry.isIntersecting).forEach((entry) => {
-    const target = entry.target;
-    target.setAttribute('src', target.getAttribute('data-src'));
+if (typeof IntersectionObserver != 'undefined') {
+  window.imagesObserver = new IntersectionObserver((entries, observer) => {
+    entries.filter((entry) => entry.isIntersecting).forEach((entry) => {
+      const target = entry.target;
+      target.setAttribute('src', target.getAttribute('data-src'));
 
-    observer.unobserve(target);
+      observer.unobserve(target);
+    });
+  }, {
+    rootMargin: '-30px',
   });
-}, {
-  rootMargin: '-30px',
-});
 
-document.querySelectorAll('img[data-src]').forEach((img) => window.imagesObserver.observe(img));
+  document.querySelectorAll('img[data-src]').forEach((img) => window.imagesObserver.observe(img));
+} else {
+  setTimeout(() => {
+    document.querySelectorAll('img[data-src]').forEach((img) => img.setAttribute('src', img.getAttribute('data-src')));
+  }, 300);
+}
