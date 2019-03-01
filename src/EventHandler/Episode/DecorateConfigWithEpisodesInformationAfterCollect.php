47357c67b56501701f339cc241ed14021987f431
Nawarian <nickolas@phpsp.org.br>
2019-03-01 13:01:05 +0100

    De-duplicates recent episodes from latest episode

--- src/EventHandler/Episode/DecorateConfigWithEpisodesInformationAfterCollect.php
+++ src/EventHandler/Episode/DecorateConfigWithEpisodesInformationAfterCollect.php
@@ -29,15 +29,22 @@ class DecorateConfigWithEpisodesInformationAfterCollect implements HandlerInterf
 
     private function getLatestEpisodes(PageVariable $episodes): array
     {
+        $lastEpisode = $this->getLastEpisode($episodes);
+
         return $episodes
             ->map(function (PageVariable $episode) {
                 return $episode->category;
             })
             ->values()
             ->unique()
-            ->reduce(function (array $categories, string $category) use ($episodes) {
+            ->reduce(function (array $categories, string $category) use ($episodes, $lastEpisode) {
                 $categories[$category] = $episodes
-                    ->filter(function (PageVariable $episode) use ($category) {
+                    ->filter(function (PageVariable $episode) use ($category, $lastEpisode) {
+                        // De-duplicates last episode from recommendations list
+                        if ($episode === $lastEpisode) {
+                            return false;
+                        }
+
                         return $episode->category === $category;
                     })
                     ->sortByDesc(function (PageVariable $episode) {
