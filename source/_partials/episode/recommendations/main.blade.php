a3916d5f84fe34d02a9b44e98cfd6ac485e093a4
Nawarian <nickolas@phpsp.org.br>
2019-03-06 11:40:53 +0100

    Adjusting episodes list on home-page

--- source/_partials/episode/recommendations/main.blade.php
+++ source/_partials/episode/recommendations/main.blade.php
@@ -1,6 +1,6 @@
 @if(count($recommendations) > 0)
 <section class="recommended-episodes">
-    <h1 class="recommended-episodes__title">Para você continuar entendendo</h1>
+    <h1 class="recommended-episodes__title">{{ $title ?? 'Para você continuar entendendo' }}</h1>
 
     @include('_partials.episode.episode-card-list', [
         'title' => '',
