9f1e7a1a30aa64ef6a9b195031fe0ac32dd37c52
Nawarian <nickolas@phpsp.org.br>
2019-03-12 13:22:10 +0100

    Above-mobile layout adjustments

--- source/_partials/layout/footer/footer.blade.php
+++ source/_partials/layout/footer/footer.blade.php
@@ -1,30 +1,54 @@
 <footer class="footer">
     <nav class="footer__menu">
         <ul class="footer__menu-group">
-            <li class="footer__menu-item">Início</li>
-            <li class="footer__menu-item">Episódios</li>
-            <li class="footer__menu-item">Contato</li>
+            <li class="footer__menu-item">
+                <a class="footer__menu-item__link" href="#">
+                    Início
+                </a>
+            </li>
+            <li class="footer__menu-item">
+                <a class="footer__menu-item__link" href="#">
+                    Episódios
+                </a>
+            </li>
+            <li class="footer__menu-item">
+                <a class="footer__menu-item__link" href="#">
+                    Contato
+                </a>
+            </li>
         </ul>
         <ul class="footer__menu-group">
             <li class="footer__menu-item">
-                Facebook
+                <a class="footer__menu-item__link" href="#">
+                    Facebook
+                </a>
             </li>
             <li class="footer__menu-item">
-                Instagram
+                <a class="footer__menu-item__link" href="#">
+                    Instagram
+                </a>
             </li>
             <li class="footer__menu-item">
-                Twitter
+                <a class="footer__menu-item__link" href="#">
+                    Twitter
+                </a>
             </li>
         </ul>
         <ul class="footer__menu-group">
             <li class="footer__menu-item">
-                iTunes
+                <a class="footer__menu-item__link" href="#">
+                    iTunes
+                </a>
             </li>
             <li class="footer__menu-item">
-                Spotify
+                <a class="footer__menu-item__link" href="#">
+                    Spotify
+                </a>
             </li>
             <li class="footer__menu-item">
-                Deezer
+                <a class="footer__menu-item__link" href="#">
+                    Deezer
+                </a>
             </li>
         </ul>
     </nav>
