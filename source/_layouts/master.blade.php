2099503876063f616e6531ee4a8c45a7247d9fbc
Nawarian <nickolas@phpsp.org.br>
2019-03-12 16:59:22 +0100

    Adding favicons

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,9 +1,21 @@
+@php
+$favicon = function (string $size) use ($page): string {
+    return $page->getBaseUrl() . '/assets/images/icons/favicon-' . $size . '.png';
+};
+@endphp
+
 <!DOCTYPE html>
 <html lang="pt_BR">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <link rel="icon" href="{{ $favicon('32x32') }}" size="32x32">
+        <link rel="icon" href="{{ $favicon('192x192') }}" size="192x192">
+        <link rel="apple-touch-icon-precomposed" href="{{ $favicon('180x180') }}">
+        <meta name="msapplication-TileImage" content="{{ $favicon('270x270') }}" />
+
         @yield('head')
     </head>
     <body>
