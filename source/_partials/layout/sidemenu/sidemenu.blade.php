406c1a962eeaa38e38728e06a38a84d8309ebcfa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-18 22:39:56 +0000

    Deploy PODEntender/blog to github.com/PODEntender/blog.git:gh-pages

--- source/_partials/layout/sidemenu/sidemenu.blade.php
+++ source/_partials/layout/sidemenu/sidemenu.blade.php
@@ -1,14 +1,19 @@
 <nav class="side-menu">
     <div class="side-menu__overlay" onclick="typeof toggleMenu == 'function' && toggleMenu()"></div>
-    {{--@todo remove inline style--}}
-    <button onclick="typeof toggleMenu == 'function' && toggleMenu()" style="position: absolute; top: 0; left: 0; font-size: 15px;">
-        X
+    <button class="side-menu__close-button" onclick="typeof toggleMenu == 'function' && toggleMenu()">
+        <img class="lazy-image" data-src="{{ $page->baseUrl }}/assets/images/icons/close.svg" alt="Close" width="16">
     </button>
     <ul>
         @foreach($page->menu->items as $item => $link)
-        <li class="side-menu__item">
-            <a class="side-menu__item__link" href="{{ $page->baseUrl . $link }}">{{ $item }}</a>
-        </li>
+            <li class="side-menu__item">
+                <a class="side-menu__item__link" href="{{ $page->baseUrl . $link }}">{{ $item }}</a>
+            </li>
+        @endforeach
+        <li class="side-menu__separator"></li>
+        @foreach($page->menu->social as $item => $link)
+            <li class="side-menu__item">
+                <a class="side-menu__item__link" href="{{ $link }}" target="_blank">{{ $item }}</a>
+            </li>
         @endforeach
     </ul>
 </nav>
