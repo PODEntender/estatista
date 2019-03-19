66ab58f3480ca8e4e4557eff733349194641e60b
Nawarian <nickolas@phpsp.org.br>
2019-03-20 00:40:29 +0100

    Updates rss feed generation to use DOMDocument

--- src/EventHandler/Episode/GenerateRssFeedAfterBuild.php
+++ src/EventHandler/Episode/GenerateRssFeedAfterBuild.php
@@ -2,13 +2,10 @@
 
 namespace PODEntender\EventHandler\Episode;
 
-use FeedIo\Factory;
-use FeedIo\Feed;
-use Illuminate\Support\Collection;
 use PODEntender\EventHandler\HandlerInterface;
+use PODEntender\Feed\FeedBuilder;
 use TightenCo\Jigsaw\Jigsaw;
 use TightenCo\Jigsaw\PageVariable;
-use FeedIo\Feed\Node\ElementInterface;
 
 class GenerateRssFeedAfterBuild implements HandlerInterface
 {
@@ -16,84 +13,17 @@ class GenerateRssFeedAfterBuild implements HandlerInterface
 
     public function handle(Jigsaw $jigsaw): void
     {
-        /** @var Feed $feed */
-        $feed = $this->createFeedItems($jigsaw)
-            ->reduce(function (Feed $feed, Feed\Item $item) {
-                return $feed->add($item);
-            }, $this->createFeed($jigsaw));
+        $builder = (new FeedBuilder())
+            ->channel()
+                ->title('PODEntender')
+                ->link($jigsaw->getConfig('baseUrl'))
+                ->image($jigsaw->getConfig('meta.image'))
+                ->category($jigsaw->getConfig('meta.category'))
+                ->type('episodic')
+                ->language('pt-BR')
+                ->generator('PODEntender Static Blog');
 
-        /** @var \DOMDocument $xmlFeed */
-        $xmlFeed = Factory::create()
-            ->getFeedIo()
-            ->getStandard('rss')
-            ->getFormatter()
-            ->toDom($feed);
-
-        $xmlFeed->firstChild->setAttribute('xmlns:itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd');
-        $xmlFeed->firstChild->setAttribute('xmlns:atom', 'http://www.w3.org/2005/Atom');
-
-        $feedXmlContent = $xmlFeed->saveXML();
-        $file = implode(DIRECTORY_SEPARATOR, [$jigsaw->getDestinationPath(), self::OUTPUT_FILE]);
-        file_put_contents($file, $feedXmlContent);
-    }
-
-    private function createFeed(Jigsaw $jigsaw): Feed\NodeInterface
-    {
-        $feed = (new Feed())
-            ->addElement($this->createElement('atom:link', null, [
-                'href' => implode('/', [$jigsaw->getConfig('baseUrl'), self::OUTPUT_FILE]),
-                'rel' => 'self',
-                'type' => 'application/rss+xml',
-            ]))
-            ->setTitle($jigsaw->getConfig('meta.title'))
-            ->setLink($jigsaw->getConfig('baseUrl'))
-            ->setLastModified(date_create())
-            ->set('lastBuildDate', date(DATE_RSS))
-            ->set('language', 'pt-BR');
-
-        foreach ($this->createItunesFeed($jigsaw) as $item) {
-            $feed->addElement($item);
-        }
-
-        return $feed;
-    }
-
-    private function createItunesFeed(Jigsaw $jigsaw): array
-    {
-        return [
-            $this->createElement('itunes:summary', $jigsaw->getConfig('meta.description')),
-            $this->createElement('itunes:author', $jigsaw->getConfig('meta.creatorName')),
-            $this->createElement('itunes:explicit', 'clean'),
-            $this->createElement('itunes:image', $jigsaw->getConfig('meta.image')),
-            $this->createElement('itunes:type', 'episodic'),
-            $this->createElement('itunes:subtitle', substr($jigsaw->getConfig('meta.subtitle'), 0, 250)),
-            $this->createElement('itunes:category', $jigsaw->getConfig('meta.category')),
-            $this->createElement('itunes:owner')
-                ->addElement(
-                    $this->createElement('itunes:name', substr($jigsaw->getConfig('meta.title'), 0, 250))
-                )
-                ->addElement(
-                    $this->createElement('itunes:name', $jigsaw->getConfig('meta.email'))
-                )
-        ];
-    }
-
-    private function createElement(string $name, $value = null, array $attributes = []): ElementInterface
-    {
-        $element = new Feed\Node\Element();
-        $element->setName($name)
-            ->setValue($value);
-
-        foreach ($attributes as $name => $attribute) {
-            $element->setAttribute($name, $attribute);
-        }
-
-        return $element;
-    }
-
-    private function createFeedItems(Jigsaw $jigsaw): Collection
-    {
-        return $jigsaw
+        $jigsaw
             ->getCollection('episodes')
             ->filter(function (PageVariable $episode) {
                 return false === is_null($episode->episode['blubrry']);
@@ -101,40 +31,23 @@ class GenerateRssFeedAfterBuild implements HandlerInterface
             ->sortByDesc(function (PageVariable $episode) {
                 return $episode->episode['number'];
             })
-            ->map(function (PageVariable $episode) use ($jigsaw) {
-                $title = sprintf(
-                    'PODEntender #%s - %s',
-                    $episode->episode['number'],
-                    $episode->episode['title']
-                );
-
-                $media = new Feed\Item\Media();
-                $media->setType('audio/mpeg')
-                    ->setUrl($episode->episode['blubrry']);
-
-                $item = (new Feed\Item())
-                    ->addMedia($media)
-                    ->setTitle($title)
-                    ->setDescription($episode->episode['description'])
-                    ->setLink($episode->getUrl())
-                    ->setLastModified(\DateTime::createFromFormat('U', $episode->episode['date']));
-
-                foreach ($this->createItunesFeedItem($episode, $jigsaw) as $feedElement) {
-                    $item->addElement($feedElement);
-                }
-
-                return $item;
+            ->each(function (PageVariable $episode) use ($builder, $jigsaw) {
+                $builder->addItem()
+                    ->title($episode->episode['title'])
+                    ->link($episode->getUrl())
+                    ->author($jigsaw->getConfig('meta.creatorName'))
+                    ->summary($episode->episode['description'])
+                    ->guid($episode->getUrl())
+                    ->pubDate($episode->episode['date'])
+                    ->addEnclosure()
+                        ->url($episode->episode['blubrry'])
+                        ->length(filesize($episode->episode['blubrry']))
+                        ->type('audio/mpeg');
             });
-    }
 
-    private function createItunesFeedItem(PageVariable $episode, Jigsaw $jigsaw): array
-    {
-        return [
-            $this->createElement('itunes:subtitle', substr($episode->episode['description'], 0, 250)),
-            $this->createElement('itunes:summary', $episode->episode['description']),
-            $this->createElement('itunes:author', $jigsaw->getConfig('meta.title')),
-            $this->createElement('itunes:explicit', 'clean'),
-            $this->createElement('itunes:duration', $episode->episode['duration'] ?? '00:00:00'),
-        ];
+        $xmlFeed = $builder->close()->toXml();
+        $file = implode(DIRECTORY_SEPARATOR, [$jigsaw->getDestinationPath(), self::OUTPUT_FILE]);
+
+        file_put_contents($file, $xmlFeed);
     }
 }
