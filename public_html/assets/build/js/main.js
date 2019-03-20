cdc39cfee9a74cb6d23d5afdc4e69221df166eac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-20 18:14:11 +0000

    Deploy PODEntender/blog to github.com/PODEntender/blog.git:gh-pages

--- public_html/assets/build/js/main.js
+++ public_html/assets/build/js/main.js
@@ -1,1 +1,189 @@
-!function(e){var n={};function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:r})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,n){if(1&n&&(e=t(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(t.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var o in e)t.d(r,o,function(n){return e[n]}.bind(null,o));return r},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="/",t(t.s=0)}({0:function(e,n,t){t("BSPe"),t("olAV"),e.exports=t("Suj0")},BSPe:function(e,n){window.toggleMenu=function(){return document.querySelector(".side-menu").classList.toggle("side-menu--visible")};var t=function(e){return e.setAttribute("src",e.getAttribute("data-src"))},r=function(e){return function(n){return n.classList.remove(e)}};if("undefined"!=typeof IntersectionObserver){window.imagesObserver=new IntersectionObserver(function(e,n){e.filter(function(e){return e.isIntersecting}).forEach(function(e){var o=e.target;t(o),r("lazy-image")(o),n.unobserve(o)})},{rootMargin:"-20px"}),document.querySelectorAll("img.lazy-image").forEach(function(e){return window.imagesObserver.observe(e)});var o=document.querySelector(".episode__comments");o&&(window.commentsObserver=new IntersectionObserver(function(e,n){e.filter(function(e){return e.isIntersecting}).forEach(function(e){window.initDisqus&&window.initDisqus(),n.unobserve(e.target)})}),window.commentsObserver.observe(o))}else setTimeout(function(){console.log(document.querySelectorAll("img.lazy-image"));var e=document.querySelectorAll("img.lazy-image");e.forEach(t),e.forEach(r("lazy-image"))},400)},Suj0:function(e,n){},olAV:function(e,n){}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./source/_assets/js/main.js":
+/*!***********************************!*\
+  !*** ./source/_assets/js/main.js ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+window.toggleMenu = function () {
+  return document.querySelector('.side-menu').classList.toggle('side-menu--visible');
+};
+
+var loadImage = function loadImage(img) {
+  return img.setAttribute('src', img.getAttribute('data-src'));
+};
+
+var removeClass = function removeClass(className) {
+  return function (elm) {
+    return elm.classList.remove(className);
+  };
+};
+
+if (typeof IntersectionObserver != 'undefined') {
+  window.imagesObserver = new IntersectionObserver(function (entries, observer) {
+    entries.filter(function (entry) {
+      return entry.isIntersecting;
+    }).forEach(function (entry) {
+      var target = entry.target;
+      loadImage(target);
+      removeClass('lazy-image')(target);
+      observer.unobserve(target);
+    });
+  }, {
+    rootMargin: '-20px'
+  });
+  document.querySelectorAll('img.lazy-image').forEach(function (img) {
+    return window.imagesObserver.observe(img);
+  });
+  var commentsSection = document.querySelector('.episode__comments');
+
+  if (commentsSection) {
+    window.commentsObserver = new IntersectionObserver(function (entries, observer) {
+      entries.filter(function (entry) {
+        return entry.isIntersecting;
+      }).forEach(function (entry) {
+        if (window.initDisqus) {
+          window.initDisqus();
+        }
+
+        observer.unobserve(entry.target);
+      });
+    });
+    window.commentsObserver.observe(commentsSection);
+  }
+} else {
+  setTimeout(function () {
+    console.log(document.querySelectorAll('img.lazy-image'));
+    var images = document.querySelectorAll('img.lazy-image');
+    images.forEach(loadImage);
+    images.forEach(removeClass('lazy-image'));
+  }, 400);
+}
+
+/***/ }),
+
+/***/ "./source/_assets/sass/episode/main.scss":
+/*!***********************************************!*\
+  !*** ./source/_assets/sass/episode/main.scss ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./source/_assets/sass/main.scss":
+/*!***************************************!*\
+  !*** ./source/_assets/sass/main.scss ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*****************************************************************************************************************!*\
+  !*** multi ./source/_assets/js/main.js ./source/_assets/sass/main.scss ./source/_assets/sass/episode/main.scss ***!
+  \*****************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Users/nsilva/RG/personal/blog/source/_assets/js/main.js */"./source/_assets/js/main.js");
+__webpack_require__(/*! /Users/nsilva/RG/personal/blog/source/_assets/sass/main.scss */"./source/_assets/sass/main.scss");
+module.exports = __webpack_require__(/*! /Users/nsilva/RG/personal/blog/source/_assets/sass/episode/main.scss */"./source/_assets/sass/episode/main.scss");
+
+
+/***/ })
+
+/******/ });
