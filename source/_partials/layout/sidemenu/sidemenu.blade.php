04d08a80d96f791c2936dba588be1769a0de5555
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-20 15:16:43 +0000

    Deploy PODEntender/blog to github.com/PODEntender/blog.git:gh-pages

--- source/_partials/layout/sidemenu/sidemenu.blade.php
+++ source/_partials/layout/sidemenu/sidemenu.blade.php
@@ -1,7 +1,7 @@
 <nav class="side-menu">
     <div class="side-menu__overlay" onclick="typeof toggleMenu == 'function' && toggleMenu()"></div>
-    <button role="button" aria-label="Toggle Menu" class="side-menu__close-button" onclick="typeof toggleMenu == 'function' && toggleMenu()">
-        <img class="lazy-image" data-src="{{ $page->baseUrl }}/assets/images/icons/close.svg" alt="Close" width="16">
+    <button role="button" aria-label="Close Menu" class="side-menu__close-button" onclick="typeof toggleMenu == 'function' && toggleMenu()">
+        <img class="lazy-image" data-src="{{ $page->baseUrl }}/assets/images/icons/close.svg" alt="Close Menu" width="16">
     </button>
     <ul>
         @foreach($page->menu->items as $item => $link)
