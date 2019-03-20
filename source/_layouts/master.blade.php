fc325cc2486ee3676cc04bef909e971fb2df9aa5
Níckolas Daniel da Silva <nickolas@phpsp.org.br>
2019-03-20 15:24:15 +0100

    Updating html tag to include proper pt-BR language

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,7 +5,7 @@ $favicon = function (string $size) use ($page): string {
 @endphp
 
 <!DOCTYPE html>
-<html lang="pt_BR">
+<html lang="pt-BR">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
