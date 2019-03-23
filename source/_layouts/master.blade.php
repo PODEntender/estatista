421a0248621ad4781c880e6f9358955bf1b0b548
Nawarian <nickolas@phpsp.org.br>
2019-03-23 11:02:48 +0100

    Adding google analytics and tag manager

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,6 +7,13 @@ $favicon = function (string $size) use ($page): string {
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ $page->googleAnalyticsId }}"></script>
+        <script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '{{ $page->googleAnalyticsId }}');</script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','{{ $page->googleTagManagerId }}');</script>
+        <!-- End Google Tag Manager -->
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
