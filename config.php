89ccc3d3364aa5230a1ed6d900b97eda961c9128
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-23 10:05:27 +0000

    Deploy PODEntender/blog to github.com/PODEntender/blog.git:gh-pages

--- config.php
+++ config.php
@@ -5,6 +5,8 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 return [
     'production' => false,
     'baseUrl' => 'http://localhost:3000',
+    'googleAnalyticsId' => 'GA-TEST-ID',
+    'googleTagManagerId' => 'GTM-TEST-ID',
     'meta' => [
         'creatorName' => 'PODEntender',
         'title' => 'PODEntender',
--- config.production.php
+++ config.production.php
@@ -3,5 +3,8 @@
 return [
     'production' => true,
     // 'baseUrl' => 'https://www.podentender.com',
-    'baseUrl' => 'https://podentender.github.io/blog/public_html'
+    'baseUrl' => 'https://podentender.github.io/blog/public_html',
+
+    'googleAnalyticsId' => 'UA-126110575-1',
+    'googleTagManagerId' => 'GTM-W7MXP75',
 ];
