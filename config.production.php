d5f57fd2ec9c7f1957b9a4d89c65d98aa9e4d38d
Nawarian <nickolas@phpsp.org.br>
2019-03-26 21:55:21 +0100

    Moving production URL to beta.podentender.com

--- config.production.php
+++ config.production.php
@@ -2,8 +2,7 @@
 
 return [
     'production' => true,
-    // 'baseUrl' => 'https://www.podentender.com',
-    'baseUrl' => 'https://podentender.github.io/blog/public_html',
+    'baseUrl' => 'https://beta.podentender.com',
 
     // 'googleAnalyticsId' => 'UA-126110575-1',
     // 'googleTagManagerId' => 'GTM-W7MXP75',
