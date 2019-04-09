00990724fb2eb0af83ce66b3b8beba66c5a50602
nawarian <nickolas@phpsp.org.br>
2019-04-10 00:03:33 +0200

    Removing Url namespace and useless functions

--- config/functions.php
+++ config/functions.php
@@ -1,14 +1,9 @@
 <?php
 
 use TightenCo\Jigsaw\PageVariable;
-use PODEntender\Helper\Url;
 use PODEntender\Infrastructure\Domain\Factory\JigsawPostFactory;
 
 return [
-    'url' => new Url(),
-    'getCategoryLink' => function (PageVariable $page, string $name) {
-        return $page->getBaseUrl() . '/categoria/' . $name;
-    },
     'makePostEntity' => function (PageVariable $page) {
         $factory = new JigsawPostFactory();
 
