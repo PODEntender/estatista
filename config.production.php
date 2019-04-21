954c4bb7df585ce23aabe0f9d54aba735080496c
nawarian <nickolas@phpsp.org.br>
2019-04-21 16:25:28 +0200

    Makes GA and GTM ids environment dependent

--- config.production.php
+++ config.production.php
@@ -4,6 +4,7 @@ return [
     'production' => true,
     'baseUrl' => 'https://beta.podentender.com',
 
-    // 'googleAnalyticsId' => 'UA-126110575-1',
-    // 'googleTagManagerId' => 'GTM-W7MXP75',
+    'googleAnalyticsId' => getenv('GA_ID'),
+    'googleTagManagerId' => getenv('GTM_ID'),
 ];
+
