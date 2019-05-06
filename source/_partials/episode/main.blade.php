b1dd2b1bfc753242e8e08b1fcb1229161883a6c2
nawarian <nickolas@phpsp.org.br>
2019-05-06 20:07:49 +0200

    #26 adds sticky component for desktop

--- source/_partials/episode/main.blade.php
+++ source/_partials/episode/main.blade.php
@@ -1,5 +1,6 @@
 <article class="content">
     @include('_partials.episode.cover.main')
+    @include('_partials.components.sticky-share')
     @include('_partials.episode.player')
     <section class="paragraphs-list">
         @yield('content')
