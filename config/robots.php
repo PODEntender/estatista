709c552d31926b1f4605d695ab51f08d11b7c62c
José Filho <jose-silvafilho@hotmail.com>
2019-05-18 14:23:06 -0300

    add new configuration for robots.txt implementation

---
+++ config/robots.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'RobotsTxt' => [
+        'sitemap' => 'https://podentender.com/sitemap.xml',
+        'rulesSetCollection' => [
+            [
+                'User-Agent' => '*',
+                'Disallow' => [
+                    '*.html',
+                ],
+                'Allow' => []
+            ],
+        ],
+    ],
+];
