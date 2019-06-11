bba38e3228d2e340280a76f700cb6a6ad7c3aeef
Níckolas Daniel da Silva <nickolas@phpsp.org.br>
2019-06-11 19:16:44 +0200

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -13,6 +13,15 @@ $favicon = function (string $size) use ($page): string {
         <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','{{ $page->googleTagManagerId }}');</script>
         <!-- End Google Tag Manager -->
+        <!-- Google Adsense -->
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-3354007706896420",
+            enable_page_level_ads: true
+          });
+        </script>
+        <!-- End Google Adsense -->
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
