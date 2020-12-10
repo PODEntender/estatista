d186510038e415fcefb25ca4771cb1ef9326fad6
Níckolas Daniel da Silva <nickolas@phpsp.org.br>
2020-12-10 18:45:03 +0100

    Update config.production.php

--- config.production.php
+++ config.production.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'https://podentender.com',
+    'baseUrl' => 'https://podentender.com.br',
 
     'googleAnalyticsId' => getenv('GA_ID'),
     'googleTagManagerId' => getenv('GTM_ID'),
